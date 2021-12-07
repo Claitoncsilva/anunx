66e5881545261e199b79a0d013685664063a0268
Claitoncsilva <claiton1306@gmail.com>
2021-12-07 11:51:39 +0000

    adicionando box de preço

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -7,7 +7,12 @@ import {
     Typography,
     Select,
     Button,
-    IconButton } from '@material-ui/core'
+    IconButton,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    InputAdornment,
+} from '@material-ui/core'
 
 import { useDropzone } from 'react-dropzone'
 import { makeStyles } from '@material-ui/core/styles'
@@ -221,6 +226,25 @@ const Publish = () => {
                 </Box>    
             </Container> 
 
+
+            <Container maxWidth="md" className={classes.boxContainer} >
+                <Box className={classes.box}>
+                    <Typography component="h6" variant="h6"  color="textPrimary">
+                           Preço
+                    </Typography>
+                        <br />
+                        <FormControl fullWidth variant="outlined">
+                            <InputLabel>Valor</InputLabel>
+                            <OutlinedInput
+                                onChange={() => {}}                             
+                                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                labelWidth={40}
+                            />
+                        </FormControl>
+                </Box>    
+            </Container>          
+
+
             <Container maxWidth="md" className={classes.boxContainer} >
                 <Box className={classes.box}>
                     <Typography component="h6" variant="h6"  color="textPrimary" gutterBottom>
@@ -264,4 +288,9 @@ const Publish = () => {
 }
 
 export default Publish
+
+
+
+
+
 
